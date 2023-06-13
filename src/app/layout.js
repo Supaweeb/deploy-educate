74ebb61ba4eb9d37d5154a951d12a2439b121a6a
Supawee (Oh) Borrisutsawas <supawee@digio.co.th>
2023-06-13 22:40:13 +0700

    Add sidebar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,14 @@
 'use client'
 
 import { Inter } from 'next/font/google'
-import styled from 'styled-components';
 import '../styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
-const Body = styled.body`
-`;
-
 export default function RootLayout({ children }) {
   return (
-    <>
-      <html lang="en">
-        <Body className={inter.className}>{children}</Body>
-      </html>
-    </>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   )
 }

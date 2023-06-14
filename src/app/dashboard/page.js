6ac3805dc7efd126abb72b1810c4ea40efad4975
Supawee (Oh) Borrisutsawas <supawee@digio.co.th>
2023-06-14 14:18:15 +0700

    debug: layout.js

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,25 +1,17 @@
 'use client'
-
 import { useRouter } from 'next/navigation'
-import styled, { ThemeProvider, css } from 'styled-components'
-
-const Container = styled.div`
-  float: right;
-  width: 100%;
-  padding: 0 2em;
-`
 
 export default function Home() {
   const router = useRouter()
 
   return (
     <>
-      <div style={{ display: "block", margin: "1em" }}>
+      <body>
           <p>Dashboard</p>
-          <button type="button" onClick={() => router.push('/')}>
+          <button type="button" onClick={() => router.push('/dashboard')}>
             Dashboard
           </button>
-      </div>
+      </body>
     </>
   )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
-'use client'
+"use client"
 
 import { Inter } from 'next/font/google'
+import { usePathname  } from "next/navigation";
 import styled from 'styled-components';
+
+import Sidebar from '../components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,9 +13,24 @@ const Body = styled.body`
 `;
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+
+  if (pathname === "/") {
+    return (
+      <html lang="en">
+        <Body className={inter.className}>
+          {children}
+        </Body>
+      </html>
+    )
+  }
+
   return (
     <html lang="en">
-      <Body className={inter.className}>{children}</Body>
+      <Body className={inter.className} style={{ display: "inline-flex" }}>
+        <Sidebar/>
+        {children}
+      </Body>
     </html>
   )
 }

053293ee306c3100fef48fdf894cfdebe258446e
Supawee (Oh) Borrisutsawas <supawee@digio.co.th>
2023-06-14 13:56:29 +0700

    Update: sidebar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+"use client"
+
 import { Inter } from 'next/font/google'
 import styled from 'styled-components';
 import { usePathname  } from "next/navigation";
@@ -5,6 +7,10 @@ import { usePathname  } from "next/navigation";
 import Sidebar from '../components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const Body = styled.body`
+  margin: 0;
+`;
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
